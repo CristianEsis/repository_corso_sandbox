4b8b410ca07c9079e7dfa2da1b71423ccdc6a0dd
Cristian Chirivì <c.chirivi@esis-italia.com>
2025-10-13 09:51:45 +0200

    feat: added hello function

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def sum(x, y):
 def times(x, n):
     return x * n
 
+def hello(name):
+    return f'Hello {name}!!'
+
 print('[*] Sum Function test')
 print(f'your sum is: {sum(10, 11)}')
 print('[*] Times Function test')
 print(f'your times is: {times(10, 11)}')
+print(f'{hello('Cristian')}')
